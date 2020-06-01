61fd0b48744f191c7c19f86d91fd7db67fe8e8e8
SmithLam <lamphuoctuong@gmail.com>
2020-06-01 13:52:20 +0700

    Add in animate.css

--- src/components/YoutubeModal.js
+++ src/components/YoutubeModal.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Modal, ModalHeader, ModalTitle, ModalBody, Button } from 'react-bootstrap'
 import YouTube from '@u-wave/react-youtube';
-
+import { fadeIn, backdropStyle } from 'animate.css'
 
 export default function YoutubeModal(props) {
     return (
@@ -12,6 +12,7 @@ export default function YoutubeModal(props) {
       dialogClassName="modal-90h"
       aria-labelledby="contained-modal-title-vcenter"
       centered
+      animation="fade"
      >
       <Modal.Header id="modal-head" closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
@@ -29,16 +30,6 @@ export default function YoutubeModal(props) {
               controls
             />
           </div>
-        {/* <h4>{props.movieID.name}</h4>
-        <div className="d-flex justify-content-center">
-        <YouTube
-        video={props.movieID.key? props.movieID.key:"fT7pFSfVZBI"}
-        allowFullscreen
-        height="480px"
-        width="600px"
-        controls
-        />
-        </div> */}
       </Modal.Body>
       {/* <Modal.Footer>
         <Button onClick={props.onHide}>Close</Button>
